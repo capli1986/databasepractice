a9bdb4ab34dcbc50158e4c5dd0345982a8488e67
capli <capli@tencent.com>
2020-10-02 17:04:32 +0800

    字符串翻转，然后切割，分别是1个字符，2个字符，3个字符。。。

---
+++ DataPractice/untitled/strReverse.py
@@ -0,0 +1,23 @@
+#  -*- coding:utf8 -*-
+
+def  get_str(S):
+    if  S=="":
+        return 0
+    else:
+        #RS = reversed(S)
+        RS=''.join(reversed(S))
+        i=1
+        NS=[]
+        print  "RS",RS
+        while(i<len(S)):
+            NS.append(RS[0:i])
+            print  "NS",NS
+            S=S[0:len(S)-i]
+            print  "SSS",S
+            RS ="".join(reversed(S))
+            i=i+1
+        return  NS
+
+if  __name__== '__main__':
+    NS = get_str("aahhh")
+    print  NS
